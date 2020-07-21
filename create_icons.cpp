27d85d30f1d13b279d18a49f0692d99777c47d79
Joep Moritz <jmoritz@snap.com>
2020-07-21 18:26:46 +0100

    Adds code to generate letter icons

--- create_icons.cpp
+++ create_icons.cpp
@@ -1,4 +1,5 @@
 // Compile and run it with: g++ -I/usr/local/include/cairo create_icons.cpp -lcairo && ./a.out
+// Compile and run it with: clang -I/opt/local/include/cairo create_icons.cpp -L/opt/local/lib/cairo -lcairo
 
 #include <cairo.h>
 #include <stdio.h>
@@ -10,21 +11,21 @@
 #define BRIGHTNESS 0.7
 #define HEIGHT 15
 #define WIDTH 15
-#define PATH "icons/osx/%d.png"
+#define PATH "icons/osx/%s.png"
 #elif defined(__linux__)
 #define FONT_FACE "monospace"
 #define FONT_SIZE 12.0
 #define BRIGHTNESS 0.60
 #define HEIGHT 15
 #define WIDTH 14
-#define PATH "icons/linux/%d.png"
+#define PATH "icons/linux/%s.png"
 #else
 #define FONT_FACE "consolas"
 #define FONT_SIZE 11.0
 #define BRIGHTNESS 0.575
 #define HEIGHT 15
 #define WIDTH 14
-#define PATH "icons/windows/%d.png"
+#define PATH "icons/windows/%s.png"
 #endif
 
 void create_pngs(int surfacewidth, int surfaceheight, float fontsize, const char* format, int start=0, int end=100)
@@ -35,9 +36,10 @@ void create_pngs(int surfacewidth, int surfaceheight, float fontsize, const char
     cairo_select_font_face(cr, FONT_FACE, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
     cairo_set_font_size(cr, fontsize);
     cairo_text_extents_t extents;
-    cairo_text_extents(cr, "0123456789", &extents);
+    // cairo_text_extents(cr, "0123456789abcdefghijklmnopqrstuvwxyz", &extents);
+    cairo_text_extents(cr, "0123456789abcdefghijklmnopqrstuvwxyz", &extents);
 
-    float fontwidth = extents.width/10; // average character width
+    float fontwidth = extents.width / 36; // average character width
 
     int chars = 1;
     if (end > 10)
@@ -46,9 +48,10 @@ void create_pngs(int surfacewidth, int surfaceheight, float fontsize, const char
         chars++;
     float align = surfacewidth-chars*fontwidth;
 
+
     for (int i = start; i < end; i++)
     {
-        char buf[128];
+        char buf[4];
         sprintf(buf, "%d", i);
         cairo_set_source_rgba (cr, 0, 0, 0, 0);
         cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
@@ -57,9 +60,47 @@ void create_pngs(int surfacewidth, int surfaceheight, float fontsize, const char
         cairo_set_source_rgb(cr, BRIGHTNESS, BRIGHTNESS, BRIGHTNESS);
         cairo_move_to(cr, surfacewidth-align-strlen(buf)*fontwidth, fontsize);
         cairo_show_text(cr, buf);
-        sprintf(buf, format, i);
-        cairo_surface_write_to_png(surface, buf);
+
+        char pathbuf[128];
+        sprintf(pathbuf, format, buf);
+        cairo_surface_write_to_png(surface, pathbuf);
     }
+
+    for (int i = 0; i < 27; i++)
+    {
+        for (int j = 0; j < 26; j++)
+        {
+            char buf[4];
+
+            if (i == 26)
+            {
+                buf[0] = 97 + j;
+                buf[1] = 0;
+            }
+            else
+            {
+                buf[0] = 97 + i;
+                buf[1] = 97 + j;
+                buf[2] = 0;
+            }
+
+
+            cairo_set_source_rgba (cr, 0, 0, 0, 0);
+            cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
+            cairo_paint (cr);
+
+            cairo_set_source_rgb(cr, BRIGHTNESS, BRIGHTNESS, BRIGHTNESS);
+            cairo_move_to(cr, surfacewidth-align-strlen(buf)*fontwidth, fontsize);
+            cairo_show_text(cr, buf);
+
+            // set path
+            char pathbuf[128];
+            sprintf(pathbuf, format, buf);
+            cairo_surface_write_to_png(surface, pathbuf);
+        }
+
+    }
+
     cairo_destroy(cr);
     cairo_surface_destroy(surface);
 }
